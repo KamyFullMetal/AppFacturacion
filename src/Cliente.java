51cdd9e816e383d254796508ab7243a532ae6a84
Rosa Bernabé <al341840@uji.es>
2017-03-24 15:56:18 +0100

    Creada clase Factura

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,22 +3,23 @@ import java.util.Date;
 public class Cliente {
 	
 	/*Atributos*/
-	String nombre;
-	String NIF;
-	Direccion direccion = new Direccion();
-	String correo;
-	Date fechaAlta;
-	Tarifa tarifa = new Tarifa();
 	
+	private String nombre;
+	private String NIF;
+	private Direccion direccion = new Direccion();
+	private String correo;
+	private Date fechaAlta;
+	private Tarifa tarifa = new Tarifa();
 	
 	/*Constructor*/
-	public void cliente (String nom, String nif, Direccion direc, String correu, Date dataAlta, Tarifa tari){
+	
+	public void cliente (String nom, String nif, Direccion direc, String correu, Date dataAlta, Tarifa tarifa){
 		this.nombre = nom;
 		this.NIF = nif;
 		this.direccion = direc;
 		this.correo = correu;
 		this.fechaAlta = dataAlta;
-		this.tarifa = tari;
+		this.tarifa = tarifa;
 	}
 	
 }
