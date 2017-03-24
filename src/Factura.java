6988a1ffdbddf9bff8520078fc5001157ab4c3d5
Rosa Bernabé <al341840@uji.es>
2017-03-24 15:57:44 +0100

    fact

--- src/Factura.java
+++ src/Factura.java
@@ -1,4 +1,29 @@
+import java.util.Date;
 
 public class Factura {
+	
+	// Atributos
+	
+	private int codigo;
+	private Tarifa tarifa;
+	private Date fechaEmision;
+	private Date[] periodoFactura;
+	private float importe = getImporte();
+	
+	// Constructor
+	
+	public void Factura(int cod, Tarifa tarifa, Date fechaEmision, Date[] periodoFactura, float importe){
+		
+		this.codigo = cod;
+		this.tarifa = tarifa;
+		this.fechaEmision = fechaEmision;
+		this.periodoFactura = periodoFactura;
+		this.importe = importe;
+	}
+
+	private float getImporte() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
