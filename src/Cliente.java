d3f5e9d13ec1ba1e8d328670a0c0c1131def649e
smusaran <smusaran@gmail.com>
2017-03-24 15:29:38 +0100

    Creacion de clases Tarifa Direccion Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,23 @@
+import java.util.Date;
 
 public class Cliente {
-
+	/*Atributos*/
+	String nombre;
+	String NIF;
+	Direccion direccion = new Direccion();
+	String correo;
+	Date fechaAlta;
+	Tarifa tarifa = new Tarifa();
+	
+	
+	/*Constructor*/
+	public void cliente (String nom, String nif, Direccion direc, String correu, Date dataAlta, Tarifa tari){
+		this.nombre = nom;
+		this.NIF = nif;
+		this.direccion = direc;
+		this.correo = correu;
+		this.fechaAlta = dataAlta;
+		this.tarifa = tari;
+	}
+	
 }
